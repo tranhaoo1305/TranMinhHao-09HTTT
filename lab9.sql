4d9d3ac7eed67110751f8f3a27f3ed1a1e3f57cd
tranhaoo1305 <128454912+tranhaoo1305@users.noreply.github.com>
2023-04-18 22:37:01 +0700

    Add files via upload

---
+++ lab9.sql
@@ -0,0 +1,42 @@
+﻿use QLBanHang
+go
+---cau1---
+go
+create trigger trg_Nhap
+on Nhap
+for insert
+as
+begin
+declare @masp nvarchar(10), @manv nvarchar(10)
+declare @sln int, @dgn float 
+select @masp=masp, @manv=manv, @sln=soluongN, @dgn=dongian from inserted
+if(not exists(select * from sanpham where masp = @masp))
+begin
+raiserror(N'Không tồn tại sản phẩm trong danh mục sản phẩm',16,1)
+rollback transaction
+end
+else
+if(not exists(select * from nhanvien where manv= @manv))
+begin
+raiserror(N'Không tồn tại nhân viên có mã này',16,1)
+rollback transaction
+end
+else
+if(@sln<=0 or @dgn<=0)
+begin
+raiserror(N'Nhập sai soluong hoặc dongia', 16, 1) rollback transaction
+end
+else
+update Sanpham set soluong = soluong + @sln
+from sanpham where masp = @masp
+end
+---cau2---
+
+---cau3---
+
+---cau4---
+
+---cau5---
+
+---cau6---
+
